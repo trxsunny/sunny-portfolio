0a5ac6fac01b6964c8abb453bced8e1987b75d5b
trxsunny <trxsunny72@gmail.com>
2025-02-07 14:54:48 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 document.getElementById("clickButton").addEventListener("click", function() {
     alert("Hello,Welcome to my portfolio.");
 });
+// JavaScript for Hamburger Menu Toggle
+document.getElementById("menu-toggle").addEventListener("click", function () {
+    var navLinks = document.getElementById("nav-links");
+    navLinks.classList.toggle("show");  // Toggle the 'show' class to display/hide the menu
+});
