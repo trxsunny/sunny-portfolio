db88ba83825f63dafa5a82809cff0e7a3ee33235
trxsunny <trxsunny72@gmail.com>
2025-02-08 12:00:35 +0530

    Add files via upload

---
+++ landing.js
@@ -0,0 +1,8 @@
+// Redirect to the portfolio page when "Get Started" is clicked
+document.getElementById("getStarted").addEventListener("click", function () {
+    window.location.href = "index.html"; // Redirects to your portfolio page
+});
+document.getElementById("getStarted").addEventListener("click", function () {
+    alert("Button clicked!");  // Shows an alert when the button is clicked
+    window.location.href = "index.html";
+});
