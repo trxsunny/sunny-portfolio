8b3b89021735206276e33bb764f489f040a0bee2
trxsunny <trxsunny72@gmail.com>
2025-02-08 19:20:28 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,9 @@
+// Toggle Menu
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.getElementById("menu-toggle");
+    const navLinks = document.getElementById("nav-links");
+
+    menuToggle.addEventListener("click", function () {
+        navLinks.classList.toggle("show");
+    });
+});
---
+++ script.js
@@ -0,0 +1,5 @@
+// Redirect to the portfolio page when "Get Started" is clicked
+document.getElementById("getStarted").addEventListener("click", function () {
+    alert("Button clicked!");  // Shows an alert when the button is clicked
+    window.location.href = "portfolio.html"; // Redirects to your portfolio page
+});
