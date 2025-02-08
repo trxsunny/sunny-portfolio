551ecf957da2abfd0fc2ab94ba36d6fb2b5ca181
trxsunny <trxsunny72@gmail.com>
2025-02-08 12:04:45 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Toggle Menu
-document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.getElementById("menu-toggle");
-    const navLinks = document.getElementById("nav-links");
-
-    menuToggle.addEventListener("click", function () {
-        navLinks.classList.toggle("show");
-    });
-});
